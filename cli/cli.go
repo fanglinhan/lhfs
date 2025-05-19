23c6c103fb945dba8c80fc8c9e592821dd4c5e75
fanglinhan <695640767@qq.com>
2025-05-19 09:19:29 +0800

    create project

---
+++ cli/cli.go
@@ -0,0 +1,11 @@
+/*
+ * @Author: LinhanFang
+ * @Date: 2025-05-19 09:17:56
+ * @Last Modified by: LinhanFang
+ * @Last Modified time: 2025-05-19 09:19:16
+ */
+
+package main
+
+// start server entrance
+func main() {}
